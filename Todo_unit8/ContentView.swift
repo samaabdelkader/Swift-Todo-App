914f6511b394ae8881001229fe844bc678ce9602
Sama Abdelkader <stuf253830@ese.gov.ae>
2023-07-31 10:38:00 +0400

    Initial Commit

---
+++ Todo_unit8/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Todo_unit8
+//
+//  Created by Sama Abdelkhader on 31/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo_unit8/Todo_unit8App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_unit8App.swift
+//  Todo_unit8
+//
+//  Created by Sama Abdelkhader on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_unit8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
